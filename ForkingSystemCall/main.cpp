d886601e926613eaa37afed1bf2e7febd8244395
Randy Do <randyydoo@csu.fullerton.edu>
2023-02-27 13:35:45 -0800

    added tests

--- ForkingSystemCall/main.cpp
+++ ForkingSystemCall/main.cpp
@@ -1,8 +1,11 @@
+#include <iostream>
 #include <string>
-#include <stdio.h>
 #include <sys/types.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <cstring>
 // Consts
 const size_t BUFFER_SIZE = 8192;
 
@@ -11,30 +14,65 @@ const size_t BUFFER_SIZE = 8192;
 /// You may or may not want to solve the assignment this way.
 ///	Feel free to erase the following stubs and implement your own design instead (just make sure you pass your tests).
 bool prompt();
-void executeCommand(std::string command);
+void executeCommand(char command[BUFFER_SIZE]);
 
 
 int main()
 {
-	 pid_t process_id;
-   //set = to fork
-   process_id = fork();
-   if(process_id == 0) { execlp("/bin/ls", "ls", NULL);}
-   while(process_id > 0){
-       wait(NULL);
-       printf("Child process finished");
-	   exit(0);
-   } return 0;
+std::cout << "Hello, my name is Randy Do. Welcome to my forker! \n";
+char command[BUFFER_SIZE];
+bool exit_ = false;
 
+while(!exit_){
+   std::cout << "\n" << "cmd> ";
+   std::cin.getline(command, BUFFER_SIZE);
+   
+   //check for exit
+   if(strcmp(command, "exit") == 0) {
+      std::cout << "Thank you for using my forker!\n";
+      break;
+   }
+
+   //echo
+   std::cout << "You entered: " << command << std::endl;
+   executeCommand(command);
+   std::cout << "Thanks for using my forker!\n";
+   }
+return 0;
 }
+
+
 
 bool prompt()
 {
-	return false;
+   return true;
 }
 
-void executeCommand(std::string command)
+void executeCommand(char command[BUFFER_SIZE])
 {
-	
+int result = 0;
+pid_t pid;
+pid = fork();
+
+//child process
+if(pid == 0) { 
+   result = execlp(command, command, (char*)NULL);
+   if(result < 0){ 
+      std::cout << "Failed to execute command: " << command << " \nNo such file or directory\n";
+      exit(0);
+   }
 }
 
+//back to parent
+else if(pid > 0){
+   result = wait(NULL);
+   if(result < 0){
+      std::cout << "Failed to wait\n";
+   }
+}
+
+//error
+else{ 
+perror("Failed to fork child");
+ }
+}
